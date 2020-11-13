dcaca7e900a1efd83fc92466271f87b71443c49a
alonsofritz <alonsofritz@hotmail.com>
2020-11-13 16:07:38 -0300

    feat(ui/ux/component): create and configure page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import GlobalStyles from './styles/global';
+import { Main, Section, Sticky } from './styles';
+
+import FirstAndSecond from './components/FirstAndSecond';
+
 function App() {
   return (
     <>
-      <h1>Clone Typeform Homepage</h1>
+      <GlobalStyles />
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%'}}>
+          <FirstAndSecond />
+          <Sticky className="first" />
+          <Sticky className="second" />
+        </Section>
+
+        <Section style={{ height: '9.7%'}}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: '10.1%'}}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
     </>
   );
 }
---
+++ src/components/FirstAndSecond/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const FirstAndSecond: React.FC = () => {
+    return (
+        <Container>
+            <h1>FirstAndSecond</h1>
+        </Container>
+    )
+};
+
+export default FirstAndSecond;
